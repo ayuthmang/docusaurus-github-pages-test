c5a34741c27d700ff054212a9297af9ecd575fee
Ayuth Mangmesap <ayuth.mang@gmail.com>
2022-12-19 23:38:39 +0700

    remove baby

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-              BABY IS HERE, there
+              BABY IS NOT HERE
             Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
