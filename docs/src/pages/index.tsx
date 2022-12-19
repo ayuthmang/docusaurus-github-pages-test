71c19e53b6dab901781de18e06ded1d03ba1cee9
Ayuth Mangmesap <ayuth.mang@gmail.com>
2022-12-19 13:44:30 +0700

    little change was made

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -18,6 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
+              BABY IS HERE
             Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
